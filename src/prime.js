2d2b69e34bf2ad2ba19474774c2237c3fcbcbcb6
Oydar <free_43@mail.ru>
2023-01-23 17:09:23 +0300

    fix prime.js

--- src/prime.js
+++ src/prime.js
@@ -3,7 +3,7 @@ import welcome from './welcome.js';
 import getRandomNumber from './getRandomNumber.js';
 
 const username = welcome();
-console.log(`Answer "yes" if given number is prime. Otherwise answer "no".`);
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
 const isPrime = (num) => {
   for (let i = 2; i < Math.sqrt(num); i += 1) {
@@ -12,7 +12,7 @@ const isPrime = (num) => {
     }
   }
   return true;
-}
+};
 
 const game = (name) => {
   for (let i = 0; i < 3; i += 1) {
@@ -21,18 +21,18 @@ const game = (name) => {
     if (isPrime(questionNumber)) {
       correctAnswer = 'yes';
     } else {
-      correctAnswer = 'no'
+      correctAnswer = 'no';
     }
     console.log(`Question: ${questionNumber}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer === correctAnswer) {
       console.log('Correct!');
     } else {
-      return `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`
+      return `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${name}!`;
     }
   }
-  return `Congratulations, ${name}!`
-}
+  return `Congratulations, ${name}!`;
+};
 
 export default () => {
   console.log(game(username));
