a7c7c584b712ffad98193f3442e44c63d234e8a3
Oydar <free_43@mail.ru>
2023-01-26 15:48:43 +0300

    fix src/progression.js

--- src/progression.js
+++ src/progression.js
@@ -1,46 +1,53 @@
-import readlineSync from 'readline-sync';
-import welcome from './welcome.js';
-import getRandomNumber from './getRandomNumber.js';
+import { welcome, game } from './engine.js';
+import { getRandomNumber } from './lib.js';
 
 const username = welcome();
+const condition = 'What number is missing in the progression?';
 
-const game = (name) => {
-  for (let j = 3; j > 0; j -= 1) {
-    const firstNumberOfProgression = getRandomNumber();
-    const diffOfProgression = getRandomNumber();
-    const arrayOfNumbers = [];
-    let nextNumberOfProgression = firstNumberOfProgression;
+const generateQuestion = () => {
+  let nextNumOfProg = getRandomNumber(1, 10);
+  const diffOfProgression = getRandomNumber(1, 10);
+  let question = '';
+  const arrayOfNumbers = [];
 
-    for (let i = 10; i > 0; i -= 1) {
-      nextNumberOfProgression += diffOfProgression;
-      arrayOfNumbers.push(nextNumberOfProgression);
-    }
+  for (let i = 10; i > 0; i -= 1) {
+    nextNumOfProg += diffOfProgression;
+    arrayOfNumbers.push(nextNumOfProg);
+  }
 
-    const randomIndexOfArray = Math.floor(Math.random() * 10);
-    let str = 'Question: ';
-
-    for (let i = 0; i < 10; i += 1) {
-      if (i === randomIndexOfArray) {
-        str += '.. ';
-      } else {
-        str += `${arrayOfNumbers[i]} `;
-      }
-    }
-
-    console.log(str);
-
-    const userAnswer = readlineSync.question('Your answer: ');
-
-    if (userAnswer === `${arrayOfNumbers[randomIndexOfArray]}`) {
-      console.log('Correct!');
+  const randomIndex = getRandomNumber(0, arrayOfNumbers.length - 1);
+  for (let i = 0; i < 10; i += 1) {
+    if (i === randomIndex) {
+      question += '.. ';
     } else {
-      return `'${userAnswer}' is wrong answer ;(. Correct answer was '${arrayOfNumbers[randomIndexOfArray]}'.\nLet's try again, ${name}!`;
+      question += `${arrayOfNumbers[i]} `;
     }
   }
-  return `Congratulations, ${name}!`;
+
+  return question;
 };
 
-export default () => {
-  console.log('What number is missing in the progression?');
-  console.log(game(username));
+const isCorrect = (question) => {
+  const questionParts = question.split(' ');
+  let correct = 0;
+
+  questionParts.pop();
+
+  const index = questionParts.indexOf('..');
+
+  for (let i = 0; i < questionParts.length; i += 1) {
+    if (i !== index) {
+      questionParts[i] = Number(questionParts[i]);
+    }
+  }
+
+  if ((index === 0) || (index === 1)) {
+    correct = questionParts[1] - (questionParts[2] - questionParts[1]);
+  } else {
+    correct = questionParts[index - 1] + (questionParts[index - 1] - questionParts[index - 2]);
+  }
+
+  return String(correct);
 };
+
+export default () => game(username, condition, generateQuestion, isCorrect);
