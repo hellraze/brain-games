83848f0103d61ce1a847033f21d476d227ad48eb
Oydar <free_43@mail.ru>
2023-01-26 15:41:21 +0300

    fix src/prime.js

--- src/prime.js
+++ src/prime.js
@@ -7,7 +7,7 @@ const condition = 'Answer "yes" if given number is prime. Otherwise answer "no".
 const generateQuestion = () => String(getRandomNumber(1, 100));
 
 const isCorrect = (question) => {
-  let num = Number(question)
+  const num = Number(question);
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
       return 'no';
