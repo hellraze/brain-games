9f6420046f788ac42ebcddda04bd11beeb8b59dc
Oydar <free_43@mail.ru>
2023-01-30 10:57:49 +0300

    fix calc.js

--- games/calc.js
+++ games/calc.js
@@ -1,35 +1,30 @@
-import getRandomNumber from './lib.js';
-import { welcome, game } from './engine.js';
+import getRandomNumber from '../src/lib.js';
+import game from '../src/engine.js';
 
-const username = welcome();
 const condition = 'What is the result of the expression?';
 
-const generateQuestion = () => {
+const generateInfo = () => {
+  const result = [];
   const operators = ['+', '-', '*'];
   const number1 = getRandomNumber(1, 10);
   const number2 = getRandomNumber(1, 10);
   const operator = operators[getRandomNumber(0, operators.length - 1)];
 
-  return `${number1} ${operator} ${number2}`;
-};
-
-const isCorrect = (question) => {
-  const questionParts = question.split(' ');
-  const number1 = Number(questionParts[0]);
-  const number2 = Number(questionParts[2]);
-  const operator = questionParts[1];
-
+  result.push(`${number1} ${operator} ${number2}`);
   switch (operator) {
     case '+':
-      return String(number1 + number2);
+      result.push(String(number1 + number2));
+      break;
     case '-':
-      return String(number1 - number2);
+      result.push(String(number1 - number2));
+      break;
     case '*':
-      return String(number1 * number2);
+      result.push(String(number1 * number2));
+      break;
     default:
       break;
   }
-  return null;
+  return result;
 };
 
-export default () => game(username, condition, generateQuestion, isCorrect);
+export default () => game(condition, generateInfo);
