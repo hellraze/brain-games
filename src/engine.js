1cee8280eeb40e7e4285378a6df2c40bb105a4ea
Oydar <free_43@mail.ru>
2023-01-30 10:45:46 +0300

    fix engine.js

--- src/engine.js
+++ src/engine.js
@@ -1,13 +1,15 @@
 import readlineSync from 'readline-sync';
 
-export default (questionFunc, answerFunc) => {
+export default (condition, generateInfo) => {
   console.log('Welcome to the Brain Games!');
   const username = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${username}`);
+  console.log(condition);
 
   for (let i = 0; i < 3; i += 1) {
-    const question = questionFunc();
-    const correctAnswer = answerFunc(question);
+    const gameInfo = generateInfo();
+    const question = gameInfo[0];
+    const correctAnswer = gameInfo[1];
 
     console.log(`Question: ${question}`);
 
