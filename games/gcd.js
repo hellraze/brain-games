55edb3a70f4c1e001a520aaad00726a034a4b1c4
Oydar <free_43@mail.ru>
2023-01-30 11:16:25 +0300

    fix gcd.js

--- games/gcd.js
+++ games/gcd.js
@@ -1,32 +1,34 @@
-import { welcome, game } from './engine.js';
-import getRandomNumber from './lib.js';
+import game from '../src/engine.js';
+import getRandomNumber from '../src/lib.js';
 
-const username = welcome();
 const condition = 'Find the greatest common divisor of given numbers.';
 
-const generateQuestion = () => {
+const gcd = (num1, num2) => {
+  let max = Math.max(num1, num2);
+  let min = Math.min(num1, num2);
+  let result = min;
+
+  while (max % result > 0) {
+    result = max % result;
+    max = min;
+    min = result;
+  }
+
+  return String(result);
+};
+
+const generateInfo = () => {
+  const result = [];
   const num1 = getRandomNumber(1, 10);
   const num2 = getRandomNumber(1, 10);
 
-  return `${num1} ${num2}`;
+  result.push(`${num1} ${num2}`);
+
+  const answer = gcd(num1, num2);
+
+  result.push(answer);
+
+  return result;
 };
 
-const isCorrect = (question) => {
-  const questionParts = question.split(' ');
-  const num1 = Number(questionParts[0]);
-  const num2 = Number(questionParts[1]);
-
-  let max = Math.max(num1, num2);
-  let min = Math.min(num1, num2);
-  let gcd = min;
-
-  while (max % gcd > 0) {
-    gcd = max % gcd;
-    max = min;
-    min = gcd;
-  }
-
-  return String(gcd);
-};
-
-export default () => game(username, condition, generateQuestion, isCorrect);
+export default () => game(condition, generateInfo);
