340610f6e08150133becf08ebb91b3c6abb0c374
Oydar <free_43@mail.ru>
2023-02-01 10:58:18 +0300

    isPrime lib.js -> prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,7 +1,21 @@
 import game from '../engine.js';
-import { getRandomNumber, isPrime } from '../lib.js';
+import getRandomNumber from '../lib.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = (num) => {
+  if (num === 1) {
+    return false;
+  }
+
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+};
 
 const generateRound = () => {
   const question = getRandomNumber(1, 100);
