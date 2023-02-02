a5a14a378a6748c20345fc5e99f27d6c489b7705
Oydar <free_43@mail.ru>
2023-02-02 10:50:44 +0300

    fix evens.js

--- src/games/evens.js
+++ src/games/evens.js
@@ -5,11 +5,9 @@ const description = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 const isEven = (number) => number % 2 === 0;
 
-const correctAnswer = (question) => (isEven(question) ? 'yes' : 'no');
-
 const generateRound = () => {
   const question = String(getRandomNumber(1, 10));
-  const answer = correctAnswer(question);
+  const answer = isEven(question) ? 'yes' : 'no';
   return [question, answer];
 };
 
